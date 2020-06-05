dabcbc60ab85d094132fb72f087692e48d7b4cf9
Ahmed-10 <ahmedalaa.10@hotmail.com>
2020-06-05 17:04:44 +0200

    first commit: route planning project in cpp as part of udacity nano degree

--- build/thirdparty/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahmed/Desktop/folder/thirdparty/googletest/googlemock
-# Build directory: /home/ahmed/Desktop/folder/build/thirdparty/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
