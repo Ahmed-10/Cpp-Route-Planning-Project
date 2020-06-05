4082a1db1350b85c495b3144dcd2920cc8e94826
Ahmed-10 <ahmedalaa.10@hotmail.com>
2020-06-05 17:02:58 +0200

    first commit: C++ project as part of udacity nano degree

---
+++ build/thirdparty/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmed/Desktop/folder/thirdparty/googletest/googlemock
+# Build directory: /home/ahmed/Desktop/folder/build/thirdparty/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
